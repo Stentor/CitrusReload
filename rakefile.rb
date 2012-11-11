42eab5cc4ad74440affa91a158d04583f7213eb9
Guenael Labois <guena@guena-desktop.(none)>
2012-11-11 12:24:26 -0500

    Fixed a built conflict between Away3d and Starling

--- rakefile.rb
+++ rakefile.rb
@@ -30,26 +30,27 @@ compc "bin/CitrusReload.swc" do |t|
   t.include_sources << 'src'  
   t.source_path << 'src'
   
-  
+  t.source_path << 'extra'
   t.source_path << 'vendor/Away3D/src'
   t.source_path << 'vendor/AwayPhysics/src'
   t.source_path << 'vendor/Box2D/Source'
   t.source_path << 'vendor/Signal/src'
   t.source_path << 'vendor/Starling/starling/src'
-  t.source_path << 'extra'
   
+  t.include_sources << 'extra'
   t.include_sources << 'vendor/Away3D/src'
   t.include_sources << 'vendor/AwayPhysics/src'
   t.include_sources << 'vendor/Box2D/Source'
   t.include_sources << 'vendor/Signal/src'
-  t.include_sources << 'vendor/Starling/starling/src'
-  t.include_sources << 'extra'
+  t.include_sources << 'vendor/Starling/starling/src/starling'
+ 
+  
   
   t.include_libraries << 'vendor/Nape/lib/release_nape.swc'
   t.include_libraries << 'vendor/AwayPhysics/lib'
   
   t.warnings = false
-  
+  debug=true
   t.static_link_runtime_shared_libraries = true
 end
 
@@ -70,6 +71,7 @@ asdoc 'doc' do |t|
   t.source_path << 'vendor/Starling/starling/src'
   t.source_path << 'extra'
   t.library_path << 'vendor/Nape/lib/release_nape.swc'
+  t.library_path << 'vendor/AwayPhysics/lib'
 end
 desc "Generate documentation at doc"
 
